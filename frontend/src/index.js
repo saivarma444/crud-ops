bb0ab61197008e34653ed2e24f5e0cb7c65441b4
saivarma444 <saivarmakunaparaju444@gmail.com>
2025-04-21 16:32:42 +0530

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// Replace with your actual Google Client ID from Google Cloud Console
+const clientId = '331613423535-f2pqt2o1re3btj79lv0b99joc693siu4.apps.googleusercontent.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
